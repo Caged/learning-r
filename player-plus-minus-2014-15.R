11a4fbf338a68d626c868f1b1395c2569f5dadc3
Justin Palmer <justin@github.com>
2014-12-20 10:00:57 -0800

    remove code that we're not using

--- player-plus-minus-2014-15.R
+++ player-plus-minus-2014-15.R
@@ -2,21 +2,23 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
+# Load CSV data
 pm_data <- read.csv('data/player_plus_minus.csv')
 
+# Remove player first names to shorten labels
 pm_data$short_name <- gsub("^[a-z]+", "", pm_data$player_name, perl=TRUE, ignore.case=T)
 
-pl_data <- group_by(pm_data, team_abbreviation, player_name) %>%
-  summarize(mean = mean(plus_minus), max = max(plus_minus), min = min(plus_minus), sd = sd(plus_minus)) %>%
-  arrange(desc(mean))
-team_data <- group_by(pm_data, team_abbreviation, player_name)
-
-blazers <- pm_data %>% filter(team_abbreviation == 'CLE')
+# Sort short names.  Think this is wrong?
 pm_data$short_name <- factor(pm_data$short_name, levels = unique(pm_data$short_name))
 
 
-ggplot(pm_data, aes(x=plus_minus, y=short_name)) + 
-  geom_boxplot(fill="#6B46CA", color="#462990") + 
+# Draw a grid of teams and players
+#
+# TODO: How do I sort the players by mean plus_minus?
+# TODO: How do I change the confidence band of the box plot?
+# TODO: How do I change the color of negative values?
+ggplot(pm_data, aes(x=plus_minus, y=reorder(short_name, plus_minus, mean))) +
+  geom_boxplot(fill="#6B46CA", color="#462990", outlier.shape=NA) +
   ggtitle('Individual Plus/Minus per team') +
   ylab('Player') +
   xlab('Plus-Minus 25th-75th percentile range') +
